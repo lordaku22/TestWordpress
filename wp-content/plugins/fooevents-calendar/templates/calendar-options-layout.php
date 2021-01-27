59b3593372c42df57c67ad7e764e1fe0a60eb8b0
lordaku22 <76853360+lordaku22@users.noreply.github.com>
2021-01-27 10:00:18 -0300

    delet

--- wp-content/plugins/fooevents-calendar/templates/calendar-options-layout.php
+++
@@ -1,8 +0,0 @@
-<div class="wrap" id="fooevents-calendar-options-page">
-    <form method="post" action="options.php">
-        <table class="form-table">
-            <?php echo $calendar_options; ?>
-        </table>
-        <?php submit_button(); ?>
-    </form>
-</div>
--- wp-content/plugins/fooevents-calendar/templates/text-color-option.php
+++
@@ -1,7 +0,0 @@
-<div class="options_group">
-    <p class="form-field">
-       <label><?php _e('Calendar text color:', 'fooevents-calendar'); ?></label>
-       <input type="text" class="woocommerce-events-color-field" id="WooCommerceEventsTextColor" name="WooCommerceEventsTextColor" value="<?php echo esc_html($WooCommerceEventsTextColor); ?>"/>
-       <img class="help_tip" data-tip="<?php _e('Color of the calendar text for the event.', 'fooevents-calendar'); ?>" src="<?php echo plugins_url(); ?>/woocommerce/assets/images/help.png" height="16" width="16" />
-    </p>
-</div>
